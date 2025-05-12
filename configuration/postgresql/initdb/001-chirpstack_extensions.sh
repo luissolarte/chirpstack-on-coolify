f800460a6699f3ac9c195613cf77118b2792ab1d
luis <solarte@outlook.com>
2025-05-12 14:38:21 -0500

    pg host localhost \c ch

--- configuration/postgresql/initdb/001-chirpstack_extensions.sh
+++ configuration/postgresql/initdb/001-chirpstack_extensions.sh
@@ -4,6 +4,7 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     create role chirpstack with login password 'chirpstack';
     create database chirpstack with owner chirpstack;
+    \c chirpstack
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="chirpstack" <<-EOSQL
