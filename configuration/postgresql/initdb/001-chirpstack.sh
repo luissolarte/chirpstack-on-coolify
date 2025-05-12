7a3d2da705372ddac620939fcef85cedfd5e2ab3
luis <solarte@outlook.com>
2025-05-11 22:04:29 -0500

    version similar cs pheno password pg root

--- configuration/postgresql/initdb/001-chirpstack.sh
+++ configuration/postgresql/initdb/001-chirpstack.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role chirpstack with login password 'chirpstack';
+    create role chirpstack with login password 'root';
     create database chirpstack with owner chirpstack;
 EOSQL
 
